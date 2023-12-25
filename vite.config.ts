a11fe35c4b4aac037ec6e913ba17005d477853d8
eeee0717 <chentao020717Work@outlook.com>
2023-12-25 11:14:21 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -186,7 +186,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:80',
+        target: 'http://localhost:443',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
