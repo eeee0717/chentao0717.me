8dc1bce9d575e39ebadf2fdfe3befca41738208f
eeee0717 <chentao020717Work@outlook.com>
2024-12-31 10:57:02 +0800

    chore

--- unocss.config.ts
+++ unocss.config.ts
@@ -43,9 +43,11 @@ export default defineConfig({
     'col-span-2',
     'col-span-3',
     'col-span-4',
+    'col-span-5',
     'row-span-1',
     'row-span-2',
     'row-span-3',
     'row-span-4',
+    'row-span-5',
   ],
 })
